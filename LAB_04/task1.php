845b8b4a0b35e1d8d97508fe274a7317aedbd05a
Edvin Ess <edvess@taltech.ee>
2021-02-24 16:54:53 +0200

    did table, ened to add repeater and letter

--- LAB_04/task1.php
+++ LAB_04/task1.php
@@ -6,13 +6,33 @@ $arrDistances = array();
 for ($i = 0; $i<$numOfDistances; $i++){
     array_push($arrDistances, rand(1, 100));
 }
-echo "<pre>".print_r($arrDistances, true)."</pre>";
+echo print_r($arrDistances, true);
 sort($arrDistances);
-echo "<pre>".print_r($arrDistances, true)."</pre>";
+echo print_r($arrDistances, true);
 $arrMiles = array();
 for ($i = 0; $i < sizeof($arrDistances); $i++){
-    array_push($arrMiles[$arrDistances[$i]], $arrDistances);
+    if($arrDistances[$i-1] == $arrDistances[$i]){
+        $result = $arrDistances[$i] * KM_TO_MILES;
+        array_push($arrMiles[$arrDistances[$i+100]], $arrDistances[$i]);
+        $arrMiles[$arrDistances[$i+100]] = $result;
+    }
+    else{
+    array_push($arrMiles[$arrDistances[$i]], $arrDistances[$i]);
     $result = $arrDistances[$i] * KM_TO_MILES;
     $arrMiles[$arrDistances[$i]] = $result;
+    }
 }
-echo "<pre>".print_r($arrMiles, true)."</pre>";
+echo print_r($arrMiles, true);
+$counter = 0;
+foreach ($arrMiles as $index => $value) {
+    $result = $value/KM_TO_MILES;
+    if ($counter==0){
+        printf("KM\tMILES\n");
+        printf("%d\t%0.3f\n", $result, $value);
+        $counter++;
+    }
+    else{
+        printf("%d\t%0.3f\n", $result, $value);
+    }
+}
+?>
