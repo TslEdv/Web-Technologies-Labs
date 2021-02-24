064d6a5f6f0fcd1b17207fc6a9d2d9fc5bae8d12
Edvin Ess <edvess@taltech.ee>
2021-02-24 17:22:09 +0200

    started task 2

--- LAB_04/task2.php
+++ LAB_04/task2.php
@@ -1,2 +1,17 @@
 <?php
+include "incl/phonics.php";
+$text = 'data/text.txt';
+$handle = fopen($text, "r");
+function phonicsCount(){
+    $restOfContents = fread($handle, filesize($text));
+
+
+    $phonicsarray = array(
+        "A" => ""
+        "E" => ""
+        "I" => ""
+        "O" => ""
+        "U" => ""
+    )
+}
 ?>
