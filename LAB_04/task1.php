18232418170d54ab9938db5dd7a578f18631445b
Edvin Ess <edvess@taltech.ee>
2021-02-28 14:05:44 +0200

    changed getkey

--- LAB_04/task1.php
+++ LAB_04/task1.php
@@ -1,12 +1,14 @@
 <?php
 function getKey($testKey, &$arr){
 	$keyCount = 0;
-	for($i = 0; $i<sizeof($arr); $i++){
-		if ($arr[$i] === $testKey){
-			$keyCount++;
-			if ($keyCount > 1){
-				$arr[$i] .= chr(63 + $keyCount);
-			}
+	while(TRUE){
+		$i = 0;
+		if (array_key_exists($testKey, $arr)){
+			$testKey = $testKey.chr(65 + $i);
+			$i++;
+		}
+		else{
+			break;
 		}
 	}
 	return $testKey;
@@ -23,7 +25,7 @@ sort($arrDistances);
 print_r($arrDistances);
 $arrMiles = array();
 for ($i = 0; $i < sizeof($arrDistances); $i++){
-    $arrMiles[getKey($arrDistances[$i], $arrDistances)] = intval($arrDistances[$i], 10) / KM_TO_MILES;
+    $arrMiles[getKey($arrDistances[$i], $arrMiles)] = intval($arrDistances[$i], 10) / KM_TO_MILES;
 }
 print_r($arrMiles);
 printf("%10s%10s\n\r", "KM", "MILES");
