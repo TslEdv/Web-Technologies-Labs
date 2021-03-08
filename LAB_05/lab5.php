74a598cf5536d07b393618fa9fcf7d732020a821
Edvin Ess <edvess@taltech.ee>
2021-03-08 17:55:30 +0200

    updated some errors

--- LAB_05/lab5.php
+++ LAB_05/lab5.php
@@ -28,7 +28,7 @@
         <option value="dr">Dr</option>
     </select><br>
     <label for="age">Age:</label>
-    <input type="number" id="age" name="age" min="18" pattern="[0-9]+" required><br>
+    <input type="number" id="age" name="age" min=18 pattern="[0-9]+" required><br>
     <label for="email">Email:</label>
     <input type="email" id="email" name="email" placeholder="Enter your email" required><br>
     <label for="phone">Enter a phone number:</label>
@@ -42,7 +42,7 @@
 <?php
 if(isset($_POST['submit'])){
     foreach($_REQUEST as $var){
-        if(str_contains($var, ",")){
+        if(strpos($var, ",") !== false){
             exit("Invalid input!");
         }
     }
@@ -50,7 +50,7 @@ if(isset($_POST['submit'])){
         if(isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['age']) && isset($_POST['email']) && isset($_POST['arrival'])){
             $email_pattern = "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^";
             $date_pattern = '/^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/';
-            if (!preg_match("/^[a-zA-z-.]*$/", $_POST['firstname']) || !preg_match("/^[a-zA-z-.]*$/", $_POST['lastname']) || !preg_match ($email_pattern, $_POST['email']) || !preg_match($date_pattern, $_POST['arrival'])){
+            if (!preg_match("/^[a-zA-z-.]*$/", $_POST['fname']) || !preg_match("/^[a-zA-z-.]*$/", $_POST['lname']) || !preg_match ($email_pattern, $_POST['email']) || !preg_match($date_pattern, $_POST['arrival'])){
                 echo "Wrong input!";
             }
             else if ($_POST['age'] < 18){
