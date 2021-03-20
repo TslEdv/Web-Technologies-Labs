4e2341346eb58683cda077d96b517eacea8e2e09
Edvin Ess <edvess@taltech.ee>
2021-03-20 23:17:33 +0200

    updated some functionality

--- LAB_06/index.php
+++ LAB_06/index.php
@@ -5,23 +5,14 @@ class Course {
     public $ects;
     public $term;
 }
-$courses = array();
-$csvline = explode(PHP_EOL, file_get_contents("data/courses.csv"));
-foreach ($csvline as $line) {
-    $course = new Course;
-    $parameters = explode(";", $line);
-    $course->code = $parameters[0];
-    $course->name = $parameters[1];
-    $course->ects = $parameters[2];
-    $course->term = $parameters[3];
-    array_push($courses, $course);
-}
 class CourseActions{
     function filter($id){ // add decending order as well
         if (strlen($id) > 3){
             exit("ID TOO LONG");
         } else{
+            $id = strtoupper($id);
             $filcourses = array();
+            $courses = generateCourses();
             foreach($courses as $course){
                 if (substr($course->code, 0, 3) == $id){
                     array_push($filcourses, $course);
@@ -31,13 +22,34 @@ class CourseActions{
         }
     }
 }
+function generateCourses(){
+    $courses = array();
+    $csvline = explode(PHP_EOL, file_get_contents("data/courses.csv")); 
+    foreach ($csvline as $line) {
+        $course = new Course;
+        $parameters = explode(";", $line);
+        $course->code = $parameters[0];
+        $course->name = $parameters[1];
+        $course->ects = $parameters[2];
+        $course->term = $parameters[3];
+        array_push($courses, $course);
+    }
+    return $courses;
+}
+function getCourseTable(){
+    if (isset($_GET["query"])){
+        $action = new CourseActions;
+        $filtered = $action -> filter($_GET["query"]);
+        return $filtered;
+    } else{
+        $array = generateCourses();
+        return $array;
+    }
+}
 require_once('lib/tpl.class.php');
 const TEMPLATE_PATH = "templates";
 $t = new Template(TEMPLATE_PATH."/index_tpl.php");
-function getCourseTable(){
-    return $courses;
-}
-$tableHead = ["code", "name", "points", "semester"];
+$tableHead = ["Code", "Name", "Points", "Semester"];
 $t -> assign("title", "Courses");
 $t -> assignTable("table", getCourseTable(), $tableHead);
 echo $t -> render();
--- LAB_06/templates/index_tpl.php
+++ LAB_06/templates/index_tpl.php
@@ -5,6 +5,7 @@
 </head>
 <body>
     <h1>{TITLE}</h1>
+    <h1>You can filter by adding query or semester to the link<h1>
     <div>{TABLE}</div>
 </body>
 </html>
