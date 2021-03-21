723c49264df77ea26710a6b82bc8f824918206fc
Edvin Ess <edvess@taltech.ee>
2021-03-21 15:17:58 +0200

    added link example to template

--- LAB_06/templates/index_tpl.php
+++ LAB_06/templates/index_tpl.php
@@ -6,6 +6,7 @@
 <body>
     <h1>{TITLE}</h1>
     <h3>You can filter by adding query or semester to the link</h3>
+    <h4>Example: ?query=ICA&semester=1 </h4>
     <div>{TABLE}</div>
 </body>
 </html>
