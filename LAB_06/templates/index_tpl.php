33d08fa4a9fa126f027a5d0fc03d17a019baff01
Edvin Ess <edvess@taltech.ee>
2021-03-21 15:33:29 +0200

    added css to table

--- LAB_06/templates/index_tpl.php
+++ LAB_06/templates/index_tpl.php
@@ -4,6 +4,7 @@
     <title>{TITLE}</title>
 </head>
 <body>
+    <link rel="stylesheet" href="styles/courses.css">
     <h1>{TITLE}</h1>
     <h3>You can filter by adding query or semester to the link</h3>
     <h4>Example: ?query=ICA&semester=1 </h4>
