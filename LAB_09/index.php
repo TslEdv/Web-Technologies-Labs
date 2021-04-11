6e553ee3af1bae6f49458a63ef3c7b013f6d2e80
Edvin Ess <edvess@taltech.ee>
2021-04-11 18:10:46 +0300

    work in progress

--- LAB_09/index.php
+++ LAB_09/index.php
@@ -7,34 +7,64 @@ function sanitizeInputVar($link, $var)
     $var = mysqli_real_escape_string($link, $var);
     return $var;
 }
-function listCourses($link, $semester)
+function listCourses($link, $semester, $search, $column, $order, $reset)
 {
-    $semester = intval(sanitizeInputVar($link,$semester));
-    if(!empty($semester)){
-        $query = "SELECT C.course_code, C.course_name, C.ects_credits, S.semester_name FROM courses_201739 C LEFT JOIN semesters_201739 S ON C.Semesters_ID=S.ID WHERE C.Semesters_ID='$semester'";
-        $query = $link->prepare($query);
-        $query->execute();
-        $query->bind_result($course_code, $course_name, $ects_credits, $semester_name);
-        echo "<table>";
-        echo"<tr><th>Course Code</th><th>Course Name</th><th>Credits</th><th>Semester</th></tr>";
-        while ($query->fetch()) {
-            printf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>", $course_code, $course_name, $ects_credits, $semester_name);
-        }
-        echo "</table>";
-        $query->close();
-    } else{
-        $query = "SELECT course_code, course_name, ects_credits FROM courses_201739";
-        $query = $link->prepare($query);
-        $query->execute();
-        $query->bind_result($course_code, $course_name, $ects_credits);
-        echo "<table>";
-        echo"<tr><th>Course Code</th><th>Course Name</th><th>Credits</th></tr>";
-        while ($query->fetch()) {
-            printf("<tr><td>%s</td><td>%s</td><td>%s</td></tr>", $course_code, $course_name, $ects_credits);
-        }
-        echo "</table>";
-        $query->close();
+    $semester = intval(sanitizeInputVar($link, $semester));
+    $search = sanitizeInputVar($link, $search);
+    $column = sanitizeInputVar($link, $column);
+    $order = sanitizeInputVar($link, $order);
+    if (!empty($reset)){
+        setcookie("search", null, -1);
+        $query = "SELECT C.course_code, C.course_name, C.ects_credits, S.semester_name 
+        FROM courses_201739 C LEFT JOIN semesters_201739 S ON C.Semesters_ID=S.ID ORDER BY course_code ASC";
     }
+    else if (!empty($search)) {
+        setcookie("search", $search, '/~edvess/');
+        $query = "SELECT C.course_code, C.course_name, C.ects_credits, S.semester_name 
+        FROM courses_201739 C LEFT JOIN semesters_201739 S ON C.Semesters_ID=S.ID
+        WHERE (C.course_code LIKE '%" . $search . "%' OR C.course_name LIKE '%" . $search . "%')";
+    }
+    else if (!empty($semester)) {
+        if (isset($_COOKIE['search'])) {
+            $search = $_COOKIE['search'];
+            $query = "SELECT C.course_code, C.course_name, C.ects_credits, S.semester_name
+            FROM courses_201739 C LEFT JOIN semesters_201739 S ON C.Semesters_ID=S.ID 
+            WHERE C.Semesters_ID='$semester' 
+            AND (C.course_code LIKE '%" . $search . "%' OR C.course_name LIKE '%" . $search . "%')";
+        } else {
+            $query = "SELECT C.course_code, C.course_name, C.ects_credits, S.semester_name 
+            FROM courses_201739 C LEFT JOIN semesters_201739 S ON C.Semesters_ID=S.ID 
+            WHERE C.Semesters_ID='$semester'";
+        }
+    } else {
+            if (!empty($column)) {
+                $query = "SELECT C.course_code, C.course_name, C.ects_credits, S.semester_name 
+                FROM courses_201739 C LEFT JOIN semesters_201739 S ON C.Semesters_ID=S.ID ORDER BY " . $column . " " . $order;
+            } else {
+                $query = "SELECT C.course_code, C.course_name, C.ects_credits, S.semester_name 
+                FROM courses_201739 C LEFT JOIN semesters_201739 S ON C.Semesters_ID=S.ID ORDER BY course_code ASC";
+            }
+    }
+    $query = $link->prepare($query);
+    $query->execute();
+    $query->bind_result($course_code, $course_name, $ects_credits, $semester_name);
+    echo "<table>";
+    echo "<tr>
+    <th><a href=index.php?column=course_code&order=ASC>Course Code</a></th>
+    <th><a href=index.php?column=course_name&order=ASC>Course Name</a></th>
+    <th><a href=index.php?column=ects_credits&order=ASC>Credits</a></th>
+    <th><a href=index.php?column=semester_name&order=ASC>Semester</a></th>
+    </tr>";
+    while ($query->fetch()) {
+        printf("<tr>
+        <td>%s</td>
+        <td>%s</td>
+        <td>%s</td>
+        <td>%s</td>
+        </tr>", $course_code, $course_name, $ects_credits, $semester_name);
+    }
+    echo "</table>";
+    $query->close();
 }
 function semesters($link)
 {
@@ -61,6 +91,14 @@ function semesters($link)
     include_once "connect.db.php";
     $link = new mysqli($server, $user, $password, $database);
     semesters($link);
-    listCourses($link, $_GET['semester']);
+    ?>
+    <form action="index.php" method="POST">
+        <label for="searchfield">Search by code or name</label>
+        <input type="search" id="searchfield" name="searchfield">
+        <input type="submit" value="Search">
+        <input type="submit" value="Reset" name="Reset">
+    </form>
+    <?php
+    listCourses($link, $_GET['semester'], $_POST['searchfield'], $_GET['column'], $_GET['order'], $_POST['Reset']);
     ?>
 </body>
