8e405b18a81e4700660500a8d03eae6db84dad93
Edvin Ess <edvess@taltech.ee>
2021-04-13 20:39:14 +0300

    added localstorage reader

--- LAB_10/script.js
+++ LAB_10/script.js
@@ -17,10 +17,26 @@ function checkCookie() {
     var person = getCookie("username");
     if (person != "") {
         document.getElementById("title").innerHTML = person + "'s Shopping List";
+        var x = localStorage.length;
+        if (x != 0) {
+            for (var i = 0; i < x; i++) {
+                var y = JSON.parse(localStorage[i+1]);
+                var table = document.getElementById("shopping");
+                var length = table.rows.length;
+                var row = table.insertRow(length);
+                row.setAttribute("onclick", "remove(" + length + ")");
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                var cell3 = row.insertCell(2);
+                cell1.innerHTML = length + ".";
+                cell2.innerHTML = y.Product;
+                cell3.innerHTML = y.Quantity;
+            }
+        }
     } else {
         var person = prompt("Please enter your name", "Peeter Peet");
         if (person != "" && person != null) {
-            document.getElementById("demo").innerHTML = person + "'s Shopping List";
+            document.getElementById("title").innerHTML = person + "'s Shopping List";
             document.cookie = "username=" + person;
             sessionStorage.setItem("username", person);
         }
@@ -28,28 +44,29 @@ function checkCookie() {
 }
 function delRow(e) {
     if (e.target.matches('.delete')) {
-      let row = e.target.closest('tr');
-      row.remove();
+        let row = e.target.closest('tr');
+        row.remove();
     }
-  }
+}
 function addShopping() {
     var table = document.getElementById("shopping");
     var length = table.rows.length;
     var row = table.insertRow(length);
-    row.setAttribute("onclick", "remove(length)");
+    row.setAttribute("onclick", "remove(" + length + ")");
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     cell1.innerHTML = length + ".";
     cell2.innerHTML = document.getElementById("itemname").value;
     cell3.innerHTML = document.getElementById("quantity").value;
-    var product = '{"Product":' + '"' + document.getElementById("itemname").value +'", "Quantity":' + '"' + document.getElementById("quantity").value + '"}';
+    var product = '{"Product":' + '"' + document.getElementById("itemname").value + '", "Quantity":' + '"' + document.getElementById("quantity").value + '"}';
     var json = JSON.parse(product);
-    localStorage.setItem("ItemNr"+length, JSON.stringify(json));
+    localStorage.setItem(length, JSON.stringify(json));
 }
-function remove(lenght){
-    if(confirm("Are you sure you want to delete this item?")){
-        document.getElementById("shopping").deleteRow(lenght+1);
+function remove(lenght) {
+    if (confirm("Are you sure you want to delete this item?")) {
+        document.getElementById("shopping").deleteRow(lenght);
+        localStorage.removeItem(lenght);
     }
 }
 checkCookie();
