14025488fc4e7f73cbfe4319a899e753f7955d57
Edvin Ess <edvess@taltech.ee>
2021-04-16 22:05:56 +0300

    if name is left empty or cancelled then the name is by default unknown

--- LAB_10/script.js
+++ LAB_10/script.js
@@ -53,6 +53,7 @@ function remove(lenght) {
 }
 function logout() {
     document.cookie = "username=Monkey; expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/~edvess/;";
+    sessionStorage.removeItem("username");
     location.reload();
 }
 var person = getCookie("username");
@@ -91,7 +92,43 @@ if (person != "") {
     }
 } else {
     var person = prompt("Please enter your name", "Peeter Peet");
-    if (person != "" && person != null) {
+    if (person != "" && person !== null) {
+        document.getElementById("title").innerHTML = person + "'s Shopping List";
+        document.cookie = "username=" + person + "; path=/~edvess/;";
+        sessionStorage.setItem("username", person);
+        var x = localStorage.length;
+        if (x != 0) {
+            for (var i = 0; i < x; i++) {
+                var table = document.getElementById("shopping");
+                var length = table.rows.length;
+                if (localStorage[sessionStorage.getItem("username") + length] != null) {
+                    var y = JSON.parse(localStorage[sessionStorage.getItem("username") + length]);
+                    var row = table.insertRow(length);
+                    row.setAttribute("onclick", "remove(" + length + ")");
+                    var cell1 = row.insertCell(0);
+                    var cell2 = row.insertCell(1);
+                    var cell3 = row.insertCell(2);
+                    cell1.innerHTML = length + ".";
+                    cell2.innerHTML = y.Product;
+                    cell3.innerHTML = y.Quantity;
+                } else {
+                    if (localStorage[sessionStorage.getItem("username") + (length + 1)] != null) {
+                        var y = JSON.parse(localStorage[sessionStorage.getItem("username") + (length + 1)]);
+                        i++;
+                        var row = table.insertRow(length);
+                        row.setAttribute("onclick", "remove(" + length + ")");
+                        var cell1 = row.insertCell(0);
+                        var cell2 = row.insertCell(1);
+                        var cell3 = row.insertCell(2);
+                        cell1.innerHTML = length + ".";
+                        cell2.innerHTML = y.Product;
+                        cell3.innerHTML = y.Quantity;
+                    }
+                }
+            }
+        }
+    } else{
+        var person = "Unknown";
         document.getElementById("title").innerHTML = person + "'s Shopping List";
         document.cookie = "username=" + person + "; path=/~edvess/;";
         sessionStorage.setItem("username", person);
